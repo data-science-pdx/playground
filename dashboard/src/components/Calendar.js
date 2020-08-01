9be7020317e8ab8ba86598367cd09221b82d62f5
Mingjue <mingjue@vt.edu>
2020-08-01 10:14:08 -0700

    fix the format issues

--- dashboard/src/components/Calendar.js
+++ dashboard/src/components/Calendar.js
@@ -1,10 +1,10 @@
 import React, { useContext } from "react"
 import { Context } from "./Context"
-import utils from "./Utils"
+import Utils from "./Utils"
 import {Col, Form} from "react-bootstrap";
 
-const MIN_DATE = utils.formatDate(new Date("2020-01-01"))
-const MAX_DATE = utils.formatDate(new Date("2100-12-31"))
+const MIN_DATE = Utils.formatDate(new Date("2020-01-01"))
+const MAX_DATE = Utils.formatDate(new Date("2100-12-31"))
 
 export const Calendar = () => {
     const { startDate, setStartDate, endDate, setEndDate } = useContext(Context)
@@ -18,7 +18,7 @@ export const Calendar = () => {
                     <Form.Control type="date" id="startDate" name="trip-start"
                         value={startDate}
                         min={MIN_DATE} max={MAX_DATE}
-                        onChange={(e) => { setStartDate(utils.parseDate(e.currentTarget.value)) }}
+                        onChange={(e) => { setStartDate(Utils.parseDate(e.currentTarget.value)) }}
                     />
                 </Form.Group>
 
@@ -27,7 +27,7 @@ export const Calendar = () => {
                     <Form.Control type="date" id="endDate" name="trip-end"
                         value={endDate}
                         min={MIN_DATE} max={MAX_DATE}
-                        onChange={(e) => { setEndDate(utils.parseDate(e.currentTarget.value)) }}
+                        onChange={(e) => { setEndDate(Utils.parseDate(e.currentTarget.value)) }}
                     />
                 </Form.Group>
             </Form.Row>
